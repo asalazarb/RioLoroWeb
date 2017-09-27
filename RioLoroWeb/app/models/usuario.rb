68fca15240f197a8af9d56b0e73a38f596ceb1df
johanb21 <johanb2196@gmail.com>
2017-09-27 01:48:38 -0600

    Image uploading 90% completado

--- RioLoroWeb/app/models/usuario.rb
+++ RioLoroWeb/app/models/usuario.rb
@@ -1,3 +1,6 @@
 class Usuario < ApplicationRecord
   has_one :tipo_usuario, foreign_key: "tipo_usuario_id"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  ##validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- RioLoroWeb/db/schema.rb
+++ RioLoroWeb/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912074649) do
+ActiveRecord::Schema.define(version: 20170927015411) do
 
   create_table "accions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "accion"
@@ -108,6 +108,10 @@ ActiveRecord::Schema.define(version: 20170912074649) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
